1d937c71e5b965437436df47669b7f0b7f259be5
Vittorio <rigazilla@gmail.com>
2018-10-02 11:51:04 +0200

    test for write in ___script_cache with auth

--- test-exec/src/main/java/testExec/Main.java
+++ test-exec/src/main/java/testExec/Main.java
@@ -11,22 +11,13 @@ import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
 public class Main {
 	public static void main(String args[]) {
 		ConfigurationBuilder cb = new ConfigurationBuilder();
-		cb.addServer().port(11222).host("127.0.0.1").protocolVersion("2.4");
+		//cb.addServer().port(11222).host("127.0.0.1").protocolVersion("2.6").security().authentication().enable().saslMechanism("PLAIN").username("user").password("user");
+		cb.addServer().port(11222).host("127.0.0.1").protocolVersion("2.6");
 		try (RemoteCacheManager rcm = new RemoteCacheManager(cb.build())) {
-			RemoteCache<String, String> cache = rcm.getCache();
-			RemoteCache<String, String> namedCache = rcm.getCache("namedCache");
 			RemoteCache<String, String> scache = rcm.getCache("___script_cache");
 			String sc = "// mode=local,language=javascript\n " + "var a = \"ciao\";" + " a;";
 			scache.put("script.js", sc);
-			Map<String, ?> m = new HashMap<String, Object>();
-
-			// This completes
-			Object o = cache.execute("script.js", m);
-			System.out.println("Result is: " + o);
-
-			// This doesn't
-			o = namedCache.execute("script.js", m);
-			System.out.println("Result is: " + o);
+			System.out.println("OK");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
