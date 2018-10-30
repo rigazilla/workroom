0409cb7b54a74d627ed5836e918f99528944047a
Vittorio <rigazilla@gmail.com>
2018-10-30 17:21:29 +0100

    uncommented authentication step

--- test-exec/src/main/java/testExec/Main.java
+++ test-exec/src/main/java/testExec/Main.java
@@ -11,8 +11,7 @@ import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
 public class Main {
 	public static void main(String args[]) {
 		ConfigurationBuilder cb = new ConfigurationBuilder();
-		//cb.addServer().port(11222).host("127.0.0.1").protocolVersion("2.6").security().authentication().enable().saslMechanism("PLAIN").username("user").password("user");
-		cb.addServer().port(11222).host("127.0.0.1").protocolVersion("2.6");
+		cb.addServer().port(11222).host("127.0.0.1").protocolVersion("2.6").security().authentication().enable().saslMechanism("PLAIN").username("user").password("user");
 		try (RemoteCacheManager rcm = new RemoteCacheManager(cb.build())) {
 			RemoteCache<String, String> scache = rcm.getCache("___script_cache");
 			String sc = "// mode=local,language=javascript\n " + "var a = \"ciao\";" + " a;";
